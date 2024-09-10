ebaf98a0cf21b4dde857982715570931465d7bc2
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-10 13:29:08 -0700

    Print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def print_last(num):
+def print_last_digit(num):
     if num < 0:
-        last = num % -10
-        print(-last, end='')
+        last = num % -(10)
+        print(-(last), end='')
     else:
         last = num % 10
         print(last, end='')
