be880e621a2d76c6d4be7902ab6f76ce2a8d06d4
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-10 12:59:43 -0700

    Print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last(num):
+    if num < 0:
+        last = num % - (10)
+        print(-(last), end = '')
+    else:
+        last = num % 10
+        print(last, end='')
+    return abs(last)
