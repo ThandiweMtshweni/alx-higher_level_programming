34e1b7c605650756d0ac3a3f44015d33ea5cf94a
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-10 23:01:45 -0700

    Print combos 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print('{:02d}, '.format(number), end=' ')
-print('99')
+for i in range(100):
+    if i < 99:
+        print(f"{i:02d}, ", end="")
+    else:
+        print(f"{i:02d}")
