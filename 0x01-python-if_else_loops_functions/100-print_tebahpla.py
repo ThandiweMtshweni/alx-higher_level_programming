4f69ff04fd94c91c8214cafc30a346852929b375
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-10 23:07:24 -0700

    Print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
+for i in range(25, -1, -1):
+    print(f"{chr(122 - i) if i % 2 == 0 else chr(90 - i)}", end="")
