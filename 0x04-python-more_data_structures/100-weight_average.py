4c5cd94376227a4cab711f2c96402b8268c69652
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-18 11:03:59 -0700

    root@6729d6cb82f9:/alx-higher_level_programming/0x04-python-more_data_structures# vi 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    return sum(score * weight for score, weight in my_list) / sum(weight for _, weight in my_list)
+    if len(my_list) == 0:
+        return (0)
+    total = 0
+    div = 0
+    for item in my_list:
+        total += item[0] * item[1]
+        div += item[-1]
+    return total/div
