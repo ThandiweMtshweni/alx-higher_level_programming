37aff371b60f574b7f6ddca633ab1242181f0576
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-09-18 06:47:21 -0700

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin.python3
+
+def multiply_by_2(a_dictionary):
+    new_deco = {}
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            new_deco.update({key: value * 2})
+    return new_dico
